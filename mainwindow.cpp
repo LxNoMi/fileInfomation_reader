813df2af27854173cb643a1b5d5af442b4497a17
liuxun <1541233570@qq.com>
2019-04-14 17:59:21 +0800

    first commit

---
+++ mainwindow.cpp
@@ -0,0 +1,42 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include<QFileDialog>
+#include<QDir>
+#include<QFileInfo>
+#include<QDateTime>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QObject::connect(ui->openFileButton,SIGNAL(clicked()),this,SLOT(fileInfoSlot()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::fileInfoSlot()
+{
+    QString fileName=QFileDialog::getOpenFileName(this,"打开文件:",QDir::homePath());
+    QFileInfo fileInfo(fileName);
+    //文件大小
+    ui->fileSizeEdit->setText(QString::number(fileInfo.size())+"字节");
+    //创建时间
+    QDateTime creat=fileInfo.birthTime();
+    ui->creatTimeEdit->setText(creat.toString("yyyy-MM-dd hh:mm:ss"));
+    //最近访问时间
+    QDateTime access=fileInfo.lastRead();
+    ui->lastAccessTime->setText(access.toString("yyyy-MM-dd hh:hh:ss"));
+    //判断是否目录文件
+    if(fileInfo.isDir())
+    {
+        ui->isIssue->setText("是");
+    }
+    else
+    {
+        ui->isIssue->setText("否");
+    }
+}
---
+++ mainwindow.h
@@ -0,0 +1,25 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#pragma execution_character_set("utf-8")
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+private slots:
+    void fileInfoSlot();
+};
+
+#endif // MAINWINDOW_H
